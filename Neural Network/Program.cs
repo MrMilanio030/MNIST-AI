3c65ffdf1418dbe3dd5153dc16b88faffae76421
Milan <milanhartland@gmail.com>
2025-10-28 10:30:50 +0100

    Started work on Neural Network
    Only made the file itself

---
+++ Neural Network/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Neuron
+{
+
+}
+
+public class NeuralNetwork
+{
+    public static void Main()
+    {
+        
+    }
+}
